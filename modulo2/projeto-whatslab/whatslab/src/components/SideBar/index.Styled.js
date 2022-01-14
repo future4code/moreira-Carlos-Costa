38ec767b0636c088a10ad52eaf5aa0b48d8d2e0b
HEINRICK7 <heinrickcostta@gmail.com>
2022-01-14 17:15:32 -0300

    update project react

---
+++ modulo2/projeto-whatslab/whatslab/src/components/SideBar/index.Styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const ContainerSideBar = styled.div `
+    background-color:#CFCFCF;
+    padding: 100px 30px;
+    height: 100%;
+    
+`
+export const ContainerSideBarUl = styled.ul `
+    list-style: none;
+   
+    
+`
+export const ContainerSideBarLi = styled.li `
+    display: flex;
+    padding: 12px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+   
+    
+`
+export const ContainerSideBarIcon = styled.div `
+    display: flex;
+    padding: 2px;
+    flex-direction: row;
+    align-content: space-between;
+    justify-content: space-evenly;
+    align-items: flex-end;
+   
+    
+`
