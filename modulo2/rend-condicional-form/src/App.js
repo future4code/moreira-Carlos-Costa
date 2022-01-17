9ea016cbe2716bc08481ecd8fc09cf5dd72fd85b
HEINRICK7 <heinrickcostta@gmail.com>
2022-01-17 13:45:30 -0300

    create pages

---
+++ modulo2/rend-condicional-form/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {ContainerApp} from './Appstyled'
+
+import StepOne from './components/StepOne'
+
+
+function App() {
+  return (
+    <ContainerApp>
+     <p>Welcome</p>
+     <StepOne />
+    </ContainerApp>
+  );
+}
+
+export default App;
---
+++ modulo2/rend-condicional-form/src/Appstyled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ContainerApp = styled.div`
+    background-color:red;
+    height:100vh;
+   
+`
