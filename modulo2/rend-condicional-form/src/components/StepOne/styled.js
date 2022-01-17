3cbefff8974321a11fbd89e987068f5217a61424
HEINRICK7 <heinrickcostta@gmail.com>
2022-01-17 13:45:06 -0300

    modifield pages

---
+++ modulo2/rend-condicional-form/src/components/StepOne/styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    background-color:snow;
+    height:80vh;
+    margin: 0 auto;
+    width:70%;
+    flex-direction: column;
+    align-items: center;
+`
+export const Form = styled.div`
+    display: flex;
+    width: 100%;
+    height: 80vh;
+    flex-direction: column;
+    align-items: center;
+
+    ul {
+        list-style: none;
+        text-align: center;
+        padding: 10px;
+    }
+
+    li {
+        padding: 10px;
+    }
+
+`
