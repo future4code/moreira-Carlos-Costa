3678797365af5c2a468687e3b97366cc10e01088
HEINRICK7 <heinrickcostta@gmail.com>
2022-01-27 14:59:05 -0300

    modifield file

--- modulo2/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+
+import Header from './components/Header'
+import PlayList from './components/PlayList'
+import Player from './components/Player'
+import DetailPlayList from './components/DetailPlayList'
 
 function App() {
   return (
     <div className="App">
-      Hello Home
+      <Header />
+      <PlayList />
+      <Player />
+      <DetailPlayList />
     </div>
   );
 }
