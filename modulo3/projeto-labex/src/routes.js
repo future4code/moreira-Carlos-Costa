bb2e123095742b3fe3733e006f2d01a35b26d152
HEINRICK7 <heinrickcostta@gmail.com>
2022-02-16 20:56:03 -0300

    modifield route.js

--- modulo3/projeto-labex/src/routes.js
+++ modulo3/projeto-labex/src/routes.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home'
 import Travel from './pages/Travel'
+import Login from './pages/Login'
+import AdminHomePage from './pages/AdminHomePage'
+import NotFound from './pages/NotFound';
 
 
 const RoutesApp = () => {
@@ -12,6 +15,9 @@ const RoutesApp = () => {
             <Routes>
                 <Route path="/" element ={<Home />} />
                 <Route path="/travel" element ={<Travel />} />
+                <Route path="/login" element ={<Login />} />
+                <Route path="/detail_travel" element ={<AdminHomePage />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter> 
     );
