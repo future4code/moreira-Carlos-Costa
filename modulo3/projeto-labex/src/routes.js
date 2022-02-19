648a788ae3d49a1023df287be08adff8bd778417
HEINRICK7 <heinrickcostta@gmail.com>
2022-02-19 07:10:16 -0300

    modifield routes

--- modulo3/projeto-labex/src/routes.js
+++ modulo3/projeto-labex/src/routes.js
@@ -7,6 +7,7 @@ import Login from './pages/Login'
 import AdminHomePage from './pages/AdminHomePage'
 import NotFound from './pages/NotFound';
 import ApplicationFormPage from './pages/ApplicationFormPage'
+import CreateTripPage from './pages/CreateTripPage'
 
 
 const RoutesApp = () => {
@@ -17,8 +18,9 @@ const RoutesApp = () => {
                 <Route path="/" element ={<Home />} />
                 <Route path="/travel" element ={<Travel />} />
                 <Route path="/login" element ={<Login />} />
-                <Route path="/detail_travel" element ={<AdminHomePage />} />
+                <Route path="/admin" element ={<AdminHomePage />} />
                 <Route path="/aplication_form/:id" element ={<ApplicationFormPage />} />
+                <Route path="/create_trip" element ={<CreateTripPage />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter> 
