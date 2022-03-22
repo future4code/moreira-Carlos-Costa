342128843fe7af58f0743ee3605de49d78b510af
HEINRICK7 <heinrickcostta@gmail.com>
2022-03-22 17:01:35 -0300

    Primeiro projeto backend

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,48 @@
+
+ const operacao = (pro, x1, x2) => {
+ const arg = process.argv.slice(2);
+  
+arg[1] = Number(arg[1]);
+arg[2] = Number(arg[2]);
+  
+switch (arg[0]) {
+    case '+':
+        console.log(`Result of ${arg[1]} 
+        + ${arg[2]} = ${arg[1] + arg[2]}`);
+        break;
+  
+    case '*':
+        console.log(`Result of ${arg[1]} 
+        * ${arg[2]} = ${arg[1] * arg[2]}`);
+        break;
+  
+    case '-':
+        console.log(`Result of ${arg[1]} 
+        - ${arg[2]} = ${arg[1] - arg[2]}`);
+        break;
+  
+    case '/':
+        if (arg[2] == 0) {
+            console.log(
+                'cannot be divided by zero!!');
+        } else {
+            console.log(`Result of ${arg[1]} 
+            / ${arg[2]} = ${arg[1] / arg[2]}`);
+        }
+        break;
+  
+    case '%':
+        if (arg[2] == 0) {
+            console.log(
+                'cannot be divided by zero!!');
+        } else {
+            console.log(`Result of ${arg[1]} 
+            % ${arg[2]} = ${arg[1] % arg[2]}`);
+        }
+        break;
+  
+    default: console.log(
+        `operation cannot be performed!!`);
+}
+}
+operacao(['soma', 2,2])
