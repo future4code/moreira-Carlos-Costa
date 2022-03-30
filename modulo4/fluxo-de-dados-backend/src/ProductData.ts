8d44804f2a75cf857aa7eef51aee579635f9a3e0
HEINRICK7 <heinrickcostta@gmail.com>
2022-03-30 16:58:28 -0300

    fluxo de dados backend

---
+++ modulo4/fluxo-de-dados-backend/src/ProductData.ts
@@ -0,0 +1,27 @@
+type Product = {
+    id: number,
+    name: string,
+    price: number
+}
+export const products: Product[] = [
+	{
+		"id": 1,
+		"name": "Creme Dental Sorriso",
+		"price": 2.99
+	},
+	{
+		"id": 2,
+		"name": "Sabonete Lux",
+		"price": 1.50
+	},
+	{
+		"id": 3,
+		"name": "Shampoo Seda",
+		"price": 7.50
+	},
+	{
+		"id": 4,
+		"name": "Desodorante Monange",
+		"price": 6.75
+	}
+]
