f5a8f78e9620fd983de870c41112465f4c8bf869
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-01 17:01:12 -0300

    modifield server.ts

--- modulo4/iwfs-sistema-bancario/src/server.ts
+++ modulo4/iwfs-sistema-bancario/src/server.ts
@@ -1,20 +1,14 @@
 import express from 'express'
 
-//import router from './routes'
+import router from './routes'
 
 import { AddressInfo } from "net"
 const app = express();
 
 app.use(express.json());
-//app.use(router)
+app.use(router)
 
 const port = process.env.PORT || 3333;
-
-
-/**============================================= */
-
-/**============================================= */
-
 
 const server = app.listen(port, () => {
     if (server) {
