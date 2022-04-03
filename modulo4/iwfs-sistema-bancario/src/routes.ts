011e41a93d3a28fb7f7c56a005f050c845908efa
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-03 19:45:37 -0300

    update routes.ts

--- modulo4/iwfs-sistema-bancario/src/routes.ts
+++ modulo4/iwfs-sistema-bancario/src/routes.ts
@@ -7,5 +7,6 @@ import * as Controller from './controllers/UserController'
 routes.post('/users', Controller.userController.store)
 routes.get('/users', Controller.userController.index)
 routes.get('/users/search/cpf', Controller.userController.show)
+routes.put('/users/saldo', Controller.userController.update)
 
 export default routes;
