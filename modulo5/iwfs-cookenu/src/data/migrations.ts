10e3e7b3b749e916cc9cdf690854400d669bcaa6
HEINRICK7 <heinrickcostta@gmail.com>
2022-05-02 07:16:05 -0300

    modifield

--- modulo5/iwfs-cookenu/src/data/migrations.ts
+++ modulo5/iwfs-cookenu/src/data/migrations.ts
@@ -1,24 +1,36 @@
 
-import { connection } from "./BaseDatabase"
-
-
+import { BaseDatabase } from "./BaseDatabase"
 const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
 
-const createTables = () => connection
+const createTables = () => BaseDatabase.connection
    .raw(`
 
-      CREATE TABLE IF NOT EXISTS aula_webservices_users (
+      CREATE TABLE IF NOT EXISTS Cookenu_user (
          id VARCHAR(255) PRIMARY KEY,
          name VARCHAR(255) NOT NULL,
-         nickname VARCHAR(255) NOT NULL,
+         password VARCHAR(255) NOT NULL,
          email VARCHAR(255) UNIQUE NOT NULL,
-         address VARCHAR(255) NOT NULL
+         role VARCHAR(255) NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS Cookenu_recipes (
+         id VARCHAR(255) PRIMARY KEY,
+         title VARCHAR(255) NOT NULL,
+         description VARCHAR(255) NOT NULL,
+         date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+      
+      CREATE TABLE IF NOT EXISTS Cookenu_user_recipes (
+         user_id VARCHAR(255),
+         recipe_id VARCHAR(255),
+         FOREIGN KEY (user_id) REFERENCES Cookenu_user(id),
+         FOREIGN KEY (recipe_id) REFERENCES Cookenu_recipes(id)
       );
    `)
    .then(() => { console.log("Tabelas criadas") })
    .catch(printError)
 
-const closeConnection = () => { connection.destroy() }
+const closeConnection = () => { BaseDatabase.connection.destroy() }
 
 createTables()
   
